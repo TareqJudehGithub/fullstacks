d2166f525cb265aa9fceb0209021b7bf5e5564a8
TareqJudehGithub <tareq.joudeh@gmail.com>
2025-10-18 20:41:03 +0300

    Create a TestController, to practice the MVC data flow between Controllers, Models, and Views.

---
+++ practice/Trevoir/LeaveManagementSystem/LeaveManagementSystem/Models/TestViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagementSystem.Models
+{
+    public class TestViewModel
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
--- practice/Trevoir/LeaveManagementSystem/LeaveManagementSystem/obj/Debug/net8.0/LeaveManagementSystem.AssemblyInfo.cs
+++ practice/Trevoir/LeaveManagementSystem/LeaveManagementSystem/obj/Debug/net8.0/LeaveManagementSystem.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeaveManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+910d4d3303a0a3e6d212c70a97356204045017ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75f54bdbfd0b5a5d5eade6614e0e45342de47d38")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeaveManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeaveManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
